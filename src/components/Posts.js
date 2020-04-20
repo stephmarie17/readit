496cd4ff34dd5eead2ad47a1e81727d75e0e4b6c
Stephanie A <stephanie.m.aurelio@gmail.com>
2020-04-20 16:22:55 -0700

    fix typo in ternary expression in posts component

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -15,7 +15,7 @@ const Posts = ({ posts, loading }) => {
             {posts.map(post => (
                 <li key={post.data.id} className="post-item">
                 {/* If no image thumbnail provided, display a placeholder image */}
-                {post.data.thumbnail.length > 7 ? <a href={`https://reddit.com${post.data.permalink}`} alt="reddit post link"><img src={post.data.thumbnail} alt="thumbnail" /></a> :
+                {post.data.thumbnail.startsWith('http') ? <a href={`https://reddit.com${post.data.permalink}`} alt="reddit post link"><img src={post.data.thumbnail} alt="thumbnail" /></a> :
                 <a href={`https://reddit.com${post.data.permalink}`} alt="reddit post link"><img src="https://via.placeholder.com/100" alt="placeholder"/></a>}
                 <h4>{post.data.title}</h4>
                 <a href={`https://reddit.com${post.data.permalink}`} alt="reddit post link">Read more...</a>
